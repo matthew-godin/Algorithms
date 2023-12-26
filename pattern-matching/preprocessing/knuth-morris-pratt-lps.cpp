18cc80c9f2341cc5d5b1c246046bf73210cc3c98
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-12-26 13:59:57 -0500

    pattern-matching/preprocessing/knuth-morris-pratt-lps.cpp

---
+++ pattern-matching/preprocessing/knuth-morris-pratt-lps.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void populateLPS(vector<int> &lps, const string &pattern) {
+    int j = 0;
+    lps[0] = 0;
+    for (int i = 1; i < pattern.length();) {
+        if (pattern[i] == pattern[j]) {
+            lps[i] = ++j;
+            ++i;
+        } else {
+            if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                lps[i] = 0;
+                ++i;
+            }
+        }
+    }
+}
+
+int main() {
+    string pattern = "AAACAAAA";
+    vector<int> lps(pattern.length());
+    populateLPS(lps, pattern);
+
+    return 0;
+}
