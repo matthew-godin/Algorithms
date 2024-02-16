3cc25b8c5b8426f5cf42397ce364dba0a2cfbe22
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-02-15 19:02:04 -0500

    split strings

--- shuffling/fisher-yates.cpp
+++ shuffling/fisher-yates.cpp
@@ -6,16 +6,20 @@
 #include <algorithm>
 using namespace std;
 
+string shuffle(const string &givenString) {
+    string shuffledString = givenString;
+    for (int i = 0; i < shuffledString.size() - 1; ++i) {
+        int j = rand() % (shuffledString.size() - i) + i;
+        swap(shuffledString[i], shuffledString[j]);
+    }
+    return shuffledString;
+}
+
 int main() {
     srand(time(NULL));
     string text = "Text to Shuffle";
     
-    for (int i = 0; i < text.size() - 1; ++i) {
-        int j = rand() % (text.size() - i) + i;
-        swap(text[i], text[j]);
-    }
-    
-    cout << text << endl;
+    cout << shuffle("Text to Shuffle") << endl;
 
     return 0;
 }
